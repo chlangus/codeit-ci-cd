d969e975f8b3c8b3ead55bb4f2a49001bdd15eb3
chlangus <angusangus11@gmail.com>
2024-06-27 11:38:36 +0900

    test2

--- app/utils.ts
+++ app/utils.ts
@@ -1,9 +1,5 @@
 export function sum(a: number, b: number): number {
   const c = a + b;
   const d = a + b;
-  const e = a + b;
-  const f = a + b;
-  const g = a + b;
-  const h = a + b;
   return c;
 }
